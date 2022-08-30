89e5a883dd69bf4584c1922692d3ae06ec2f19bf
Tenzin992 <93698648+Tenzin992@users.noreply.github.com>
2022-08-29 22:50:10 -0700

    updates

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class MainProgram {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Statistics statistics = new Statistics();
         // you can write test code here
         // however, remove all unnecessary code when doing the final parts of the exercise
 
@@ -12,5 +12,14 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+        
+        statistics.addNumber(3);
+        statistics.addNumber(5);
+        statistics.addNumber(1);
+        statistics.addNumber(2);
+        System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
     }
+    
 }
